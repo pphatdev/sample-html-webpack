5a9a12dd5f3c5897b91eb77214692ebcba1da90a
pphatdev <leatsophat02@gmail.com>
2024-02-19 13:06:50 +0700

    Add Profile

---
+++ readme.md
@@ -0,0 +1,22 @@
+# My Profile
+
+## To Start Project ♻️
+
+```sh
+npm install
+```
+
+#### Starting Project 🚀
+
+```sh
+npm run start
+```
+#### --watch style 🌈
+```sh
+npm run style
+```
+
+#### Production 🚀🌈♻️
+```sh
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,71 @@
+import { loadFull } from "tsparticles";
+
+(async() => {
+    await loadFull(tsParticles);
+    await tsParticles.load({
+        id: "tsparticles",
+        options: {
+            background: {
+                color: "#000",
+                // image: "url(../profile.PNG)",
+                repeat: "no-repeat",
+                size: "40%",
+                position: "60% 50%"
+            },
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "repulse"
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "bubble"
+                    }
+                },
+                modes: {
+                bubble: {
+                    distance: 200,
+                    duration: 2,
+                    opacity: 0,
+                    size: 0,
+                    speed: 3
+                },
+                repulse: {
+                    distance: 400,
+                    duration: 0.4
+                }
+                }
+            },
+            particles: {
+                color: { value: "#ffffff" },
+                move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: "out",
+                    random: true,
+                    speed: 0.3
+                },
+                number: {
+                    density: {
+                        enable: true
+                    },
+                    value: 600
+                },
+                opacity: {
+                    animation: {
+                        enable: true,
+                        speed: 5
+                    },
+                    value: { min: 0.3, max: 0.6 }
+                },
+                shape: {
+                    type: "circle"
+                },
+                size: {
+                    value: 1
+                }
+            }
+        }
+    });
+})()
